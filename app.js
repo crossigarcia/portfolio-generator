a44e178b57a61f3993a994f9879efd512d9790de
crosgar <cecilia.rossi.garcia@gmail.com>
2021-05-14 14:21:23 -0700

    added condition to about prompt

--- app.js
+++ app.js
@@ -13,38 +13,51 @@ const inquirer = require('inquirer');
 
 const promptUser = () => {
    return inquirer.prompt([
-       {
-         type: 'input',
-         name: 'name',
-         message: 'What is your name? (Required)',
-         validate: nameInput => {
-            if (nameInput) {
-               return true;
-            } else {
-               console.log('Please enter your name!');
-               return false;
+         {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+               if (nameInput) {
+                  return true;
+               } else {
+                  console.log('Please enter your name!');
+                  return false;
+               }
+            }
+         },
+         {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+               if (githubInput) {
+                  return true;
+               } else {
+                  console.log('Please enter your username!')
+                  return false;
+               }
+            }
+
+         },
+         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would hyou like to enter some information about yourself for an "About" section?',
+            default: true
+         },
+         {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({confirmAbout}) => {
+               if (confirmAbout) {
+                  return true;
+               } else {
+                  return false;
+               }
             }
          }
-       },
-       {
-          type: 'input',
-          name: 'github',
-          message: 'Enter your GitHub Username (Required)',
-          validate: githubInput => {
-             if (githubInput) {
-                return true;
-             } else {
-                console.log('Please enter your username!')
-                return false;
-             }
-          }
-
-       },
-       {
-          type: 'input',
-          name: 'about',
-          message: 'Provide some information about yourself:'
-       }
      ]);
 };
 
