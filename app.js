4e4e5ac88a6b7f54f70d9f927d0a16d810f71742
crosgar <cecilia.rossi.garcia@gmail.com>
2021-05-16 13:48:44 -0700

    made writeFile and copyFile async, refactored code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+const {writeFile, copyFile} = require('./utils/generate-site.js');
+
 
 const promptUser = () => {
    return inquirer.prompt([
@@ -34,7 +35,7 @@ const promptUser = () => {
          {
             type: 'confirm',
             name: 'confirmAbout',
-            message: 'Would hyou like to enter some information about yourself for an "About" section?',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
             default: true
          },
          {
@@ -133,14 +134,24 @@ const promptProject = portfolioData => {
 };
 
  promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-      const pageHTML = generatePage(portfolioData);
+   .then(promptProject)
+   .then((portfolioData) => {
+     return generatePage(portfolioData);
+   })
+   .then(pageHTML => {
+      return writeFile(pageHTML);
+   })
+   .then(writeFileResponse => {
+      console.log(writeFileResponse);
+      return copyFile();
+   })
+   .then(copyFileResponse => {
+      console.log(copyFileResponse);
+   })
+   .catch(err => {
+      console.log(err);
+   });
 
-      fs.writeFile('./index.html', pageHTML, err => {
-          if (err) throw (err);
-          console.log('Portfolio complete! Check out index.html to see the output!');
-       });
        
-    });
+    
 
